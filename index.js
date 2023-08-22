8d8e3950f3d6adc2a32d259880f6f2a0dfdb82f1
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-21 22:25:51 -0400

    broncos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+]
+
+function superbowlWin(broncosRecord) {
+    const records = broncosRecord.find(record => record.result === "W"); {
+        if (records) {
+            return records.year;
+        } else {
+                    return undefined;
+                }
+            }
+        }
